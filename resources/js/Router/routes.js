3f751f46f0924f8a203a30f2304a03e3695a958a
Abdul Majid <abdulmajidcse@gmail.com>
2021-03-20 19:36:39 +0600

    arrange routes

--- resources/js/Router/routes.js
+++ resources/js/Router/routes.js
@@ -5,9 +5,26 @@ import NotFound from '../components/404'
 import { auth, guest } from './guard'
 
 const routes = [
-    { path: '/login', component: Login, name: 'login', beforeEnter: (to, from, next) => guest(to, from, next), },
-    { path: '/', component: Home, name: 'home', beforeEnter: (to, from, next) => auth(to, from, next) },
-    { path: '/about', component: About, name: 'about', beforeEnter: (to, from, next) => auth(to, from, next) },
+    {
+        path: '/login',
+        component: Login,
+        name: 'login',
+        beforeEnter: (to, from, next) => guest(to, from, next)
+    },
+
+    {
+        path: '/',
+        component: Home,
+        name: 'home',
+        beforeEnter: (to, from, next) => auth(to, from, next)
+    },
+
+    {
+        path: '/about',
+        component: About,
+        name: 'about',
+        beforeEnter: (to, from, next) => auth(to, from, next)
+    },
 
     // 404 route (fallback)
     { path: '/*', component: NotFound, name: '404' },
