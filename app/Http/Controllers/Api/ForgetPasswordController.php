db6dcaf4a74bfeefbb3d5f7166b7ee96b3e4d5a0
Abdul Majid <abdulmajidcse@gmail.com>
2021-03-23 09:31:18 +0600

    reset password

--- app/Http/Controllers/Api/ForgetPasswordController.php
+++ app/Http/Controllers/Api/ForgetPasswordController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Support\Facades\Validator;
 use App\Notifications\ResetPasswordNotification;
 
 class ForgetPasswordController extends Controller
 {
     /**
-     * custom url to redirect from email message link
+     * Sent email to get reset password link
+     * @param Illuminate\Http\Request
+     * @return json
      */
-    public $url;
-
     public function forgetPassword(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -31,11 +34,46 @@ class ForgetPasswordController extends Controller
         /**
          * custom email send with custom url
          */
-        $status = Password::sendResetLink($request->only(['email']), function($user, $token) {
-            $user->notify(new ResetPasswordNotification($this->url, $user->email, $token));
+        $status = Password::sendResetLink($request->only(['email']), function($user, $token) use($request) {
+            $user->notify(new ResetPasswordNotification($request->url, $user->email, $token));
         });
     
         return $status === Password::RESET_LINK_SENT
+                    ? response()->json(['status' => __($status)])
+                    : response()->json(['email' => __($status)], 422);
+    }
+
+    /**
+     * Reset password
+     * @param Illuminate\Http\Request
+     * @return json
+     */
+    public function resetPassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'token'    => 'required',
+            'email'    => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+    
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+    
+                $user->save();
+    
+                event(new PasswordReset($user));
+            }
+        );
+    
+        return $status == Password::PASSWORD_RESET
                     ? response()->json(['status' => __($status)])
                     : response()->json(['email' => __($status)], 422);
     }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\ForgetPasswordController;
 Route::middleware('guest:api')->group(function() {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('forgot-password', [ForgetPasswordController::class, 'forgetPassword']);
+    Route::post('reset-password', [ForgetPasswordController::class, 'resetPassword']);
 });
 
 Route::middleware('auth:api')->group(function() {
